f2a71059d3b5b766d026d0e9885229ec97ce2a83
o-zakh <mrunenlightenment@gmail.com>
2026-08-04 19:58:17 +0400

    CH1L8

--- main.go
+++ main.go
@@ -9,27 +9,12 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+
 	s := &http.Server{
 		Addr:           ":8080",
 		Handler:        mux,
 	}
 	s.ListenAndServe()
-// ----
-	// http.Handle("/foo", fooHandler)
-
-	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-
-	// s := &http.Server{
-	// 	Addr:           ":8080",
-	// 	Handler:        myHandler,
-	// 	ReadTimeout:    10 * time.Second,
-	// 	WriteTimeout:   10 * time.Second,
-	// 	MaxHeaderBytes: 1 << 20,
-	// }
-	// log.Fatal(s.ListenAndServe())
 }
-

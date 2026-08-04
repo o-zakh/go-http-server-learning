fd8e3d0b2a689157f1a4fe9a794489b566672035
o-zakh <mrunenlightenment@gmail.com>
2026-08-04 19:26:50 +0400

    CH1L4

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	// "fmt";
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	mux := http.NewServeMux()
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+	}
+	s.ListenAndServe()
+// ----
+	// http.Handle("/foo", fooHandler)
+
+	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// })
+
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// s := &http.Server{
+	// 	Addr:           ":8080",
+	// 	Handler:        myHandler,
+	// 	ReadTimeout:    10 * time.Second,
+	// 	WriteTimeout:   10 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+	// log.Fatal(s.ListenAndServe())
 }
+

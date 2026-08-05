bf4f801fd146f12becdc3a37537d14349da547a5
o-zakh <mrunenlightenment@gmail.com>
2026-08-05 18:51:52 +0400

    CH1L9

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	// mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	s := &http.Server{
 		Addr:           ":8080",

d7120f1e9509769cde34f363c13718121d3109fd
o-zakh <mrunenlightenment@gmail.com>
2026-08-05 20:56:50 +0400

    CH2L2

--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-	// "fmt";
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitsCounter(w http.ResponseWriter, r *http.Request) {
+	w.Write(fmt.Appendf([]byte{}, "Hits: %d", cfg.fileserverHits.Load()))
+}
+
+func (cfg *apiConfig) hitsReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
 
 func main() {
 
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("/metrics", apiCfg.hitsCounter)
+	mux.HandleFunc("/reset", apiCfg.hitsReset)
 
 	s := &http.Server{
-		Addr:           ":8080",
-		Handler:        mux,
+		Addr:    ":8080",
+		Handler: mux,
 	}
 	s.ListenAndServe()
 }
